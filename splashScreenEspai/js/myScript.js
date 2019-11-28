2de1120942c8855cceefba0eeba38705894d1f0d
andoal <antonio.domingo@gmail.com>
2019-11-28 12:11:12 +0100

    Add files via upload

---
+++ splashScreenEspai/js/myScript.js
@@ -0,0 +1,33 @@
+function openFullScreen(){
+    
+    
+      var elem = document.getElementById("mainScreen");
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+      }
+    
+    
+
+}
+
+function eventFire(el, etype){
+    if (el.fireEvent) {
+      el.fireEvent('on' + etype);
+    } else {
+      var evObj = document.createEvent('Events');
+      evObj.initEvent(etype, true, false);
+      el.dispatchEvent(evObj);
+    }
+  }
+  function init(){
+    eventFire(document.getElementById('mainScreen'), 'click');
+  }
+  
+  
+  
